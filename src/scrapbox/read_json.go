57b1b4051c983ec5087e05bf5c07dbaa9b28cc61
daiiz <da.iz.mg@gmail.com>
2018-04-29 00:54:19 +0900

    randomにtitleを表示する

--- src/scrapbox/read_json.go
+++ src/scrapbox/read_json.go
@@ -4,12 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	file, _ := ioutil.ReadFile("./home/data/daiiz.json")
 	var projects Project
 	json.Unmarshal(file, &projects)
-	fmt.Println(file)
-	fmt.Println(projects.Pages[0].Lines[0])
+	// fmt.Println(file)
+	sample(projects)
+}
+
+func sample(projects Project) {
+	pageLength := len(projects.Pages)
+
+	rand.Seed(time.Now().UnixNano())
+	idx := rand.Intn(pageLength)
+
+	// ランダムに記事タイトルを表示する
+	fmt.Println(projects.Pages[idx].Lines[0].Text)
 }

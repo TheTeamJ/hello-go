222760a11a24ce85f31353ce5529375aad6ebdc6
daiiz <da.iz.mg@gmail.com>
2018-04-29 01:06:49 +0900

    行数カウント

--- src/scrapbox/read_json.go
+++ src/scrapbox/read_json.go
@@ -10,18 +10,28 @@ import (
 
 func main() {
 	file, _ := ioutil.ReadFile("./home/data/daiiz.json")
-	var projects Project
-	json.Unmarshal(file, &projects)
+	var project Project
+	json.Unmarshal(file, &project)
 	// fmt.Println(file)
-	sample(projects)
+	sampleTitle(project)
+	countLines(project)
 }
 
-func sample(projects Project) {
-	pageLength := len(projects.Pages)
+func countLines(project Project) {
+	lines := 0
+	var pages = project.Pages
+	for i := 0; i < len(pages); i++ {
+		lines += len(pages[i].Lines)
+	}
+	fmt.Printf("total lines: %d\n", lines)
+}
+
+func sampleTitle(project Project) {
+	pageLength := len(project.Pages)
 
 	rand.Seed(time.Now().UnixNano())
 	idx := rand.Intn(pageLength)
 
 	// ランダムに記事タイトルを表示する
-	fmt.Println(projects.Pages[idx].Lines[0].Text)
+	fmt.Println(project.Pages[idx].Lines[0].Text)
 }
